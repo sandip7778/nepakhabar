c4ce557fc92871e0b53aa903aa6a1080d5d22dfe
Nelson Rai <raibhumihang8@gmail.com>
2024-06-08 20:22:08 +0545

    some minor changes, commited for pulling github resources

--- resources/views/admin/include/masterlayout.blade.php
+++ resources/views/admin/include/masterlayout.blade.php
@@ -191,6 +191,7 @@
             <!-- Main Content  -->
             @yield('content')
 
+
             <footer class="main-footer">
                 <div class="footer-left">
                     Copyright &copy; 2024-<?php echo date('Y'); ?> <div class="bullet"></div> Develope By <a
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,28 +6,34 @@
             <div class="section-header">
                 <h1>Profile</h1>
             </div>
-    <div class="py-2">
-        <div class="container space-y-6">
-            <div class="p-4 bg-white shadow-sm rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+            <div class="container py-2">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h5 class="card-title">Update Profile Information</h5>
+                                @include('profile.partials.update-profile-information-form')
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h5 class="card-title">Update Password</h5>
+                                @include('profile.partials.update-password-form')
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h5 class="card-title">Delete User</h5>
+                                @include('profile.partials.delete-user-form')
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div class="p-4 bg-white shadow-sm rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 bg-white shadow-sm rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
+        </section>
     </div>
-
-</section>
-</div>
 @endsection
